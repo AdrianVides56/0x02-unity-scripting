c5b0d75c3d3aaa7410a76386a2d30be43279b8c6
AdrianVides56 <adrianvj18@gmail.com>
2021-08-04 01:27:40 -0500

    Added base maze and a player (Ball)
    the player can be moved with arrow keys and `wasd`

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 275f;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float zMove = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector3(xMove, 0, zMove) * speed * Time.deltaTime;
+    }
+}
