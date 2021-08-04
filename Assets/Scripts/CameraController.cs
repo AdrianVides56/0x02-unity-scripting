eb9903f3f8e56be12f102b4b0edd19bb6a06a03f
AdrianVides56 <adrianvj18@gmail.com>
2021-08-04 15:37:58 -0500

    Added more coins and score count
    score increments by 1 every time the player picks a coin.
    The score is displayed in the console

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
-    Vector3 offset = new Vector3(0, 26, 0);
+    Vector3 offset = new Vector3(0, 26, -10);
 
+    void Start()
+    {
+        transform.Rotate(-10, 0, 0);
+    }
     // Update is called once per frame
     void Update()
     {
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 500f;
     Rigidbody rb;
+    private int score = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -19,5 +20,15 @@ public class PlayerController : MonoBehaviour
         float xMove = Input.GetAxisRaw("Horizontal");
         float zMove = Input.GetAxisRaw("Vertical");
         rb.velocity = new Vector3(xMove, 0, zMove) * speed * Time.deltaTime;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Collider>().tag == "Pickup")
+        {
+            score++;
+            Debug.Log($"Score: {score}");
+            Destroy(other.gameObject);
+        }
     }
 }
